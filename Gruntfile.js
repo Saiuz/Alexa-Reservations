77a479cc02780d3e81a8840fc705c58d25df7c07
Robert Vogel <bob@clarkvogel.com>
2015-08-04 18:05:10 -0400

    Fixed grunt file to run properly on mac

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,4 +1,4 @@
-var nwVer = '0.12.2'; //'0.10.5';  //NOTE: tried 0.11.6 & 0.12.0 but it broke the print functionality
+var nwVer = '0.12.2';
 
 var isWin32 = /^win/.test(process.platform) || nwVer === '0.10.5';
 var isWin64 = (/^win/.test(process.platform) && nwVer !== '0.10.5') && /^x64/.test(process.arch);
@@ -29,9 +29,7 @@ var nwExec = "";
 if (!isMac32 && !isMac64)
     nwExec = "cd cache/" + nwVer + "/" + os + " && nw ../../../src";
 else
-    //nwExec = "cd cache/" + nwVer + "/" + os + " && open -n -a nw ../../../src";
-    //nwExec = appDir + "/cache/" + nwVer + "/" + os + "/nwt.app/Contents/MacOS/node-webkit  /src/package.json";
-    nwExec = appDir + "/cache/" + nwVer + "/" + os + "/nw.app/Contents/MacOS/nw " + appDir + "/src";
+    nwExec = appDir + "/cache/" + nwVer + "/" + os + "/nwjs.app/Contents/MacOS/nwjs " + appDir + "/src";
 
 
 console.log("OS: " + os);
